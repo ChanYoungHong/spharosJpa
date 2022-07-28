3f1f07df50b8e06829757d022da3371416356d75
ChanYoungHong <cksdud9883@naver.com>
2022-07-29 00:20:39 +0900

    Jpa복습 및 연습

---
+++ src/main/java/com/prac/jpaprac/spharosJpa/SpharosJpaApplication.java
@@ -0,0 +1,13 @@
+package com.prac.jpaprac.spharosJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpharosJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpharosJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prac/jpaprac/spharosJpa/SpharosJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac.jpaprac.spharosJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpharosJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
